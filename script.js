ae9d8c4034d3e65b0d8587b214d3ffb2ac1d888d
Brent Abruzese <brent.abruzese@gmail.com>
2019-05-08 16:05:11 -0400

    added sql functions, new inquirer prompts, stock taking functions etc.

--- script.js
+++ script.js
@@ -13,17 +13,16 @@ connection.connect();
 function productList() {
 connection.query('SELECT * FROM products', function (error, results, fields) {
   if (error) throw error;
-  console.log('Welcome to Bamazon - A digital storefront that can only be accessed through the CLI so you feel super 31337 when using it!\n');
-  console.log('Here is whats for sale today...\n');
   for(i = 0; i < results.length; i ++) {
     console.log('id: ' + results[i].item_id + '\nproduct: ' + results[i].product_name + '\ndepartment: ' + results[i].department_name + '\nin stock: ' + results[i].stock_quantity + '\n---------------------\n');
   }
+  console.log('Welcome to Bamazon - A digital storefront that can only be accessed through the CLI (for some reason)!\n');
+  initializeStore();
 });
 
 };
 
-function intializeStore() {
-productList();
+function initializeStore() {
 inquirer.prompt([
   {
     name: 'buySelection',
@@ -36,47 +35,46 @@ inquirer.prompt([
     message: 'Great choice! How many of them would you like?'
   }
 ]).then(function(answer) {
-  let chosenProductID = answer.buySelection;
-  let chosenQty = answer.quantitySelection;
+  let chosenProductID = parseInt(answer.buySelection);
+  let chosenQty = parseInt(answer.quantitySelection);
+  let sqlSel = 'SELECT * FROM products WHERE item_id = ' + connection.escape(chosenProductID);
   switch(chosenProductID) {
-    case 1:
-      connection.query('SELECT * FROM products WHERE item_id = ?'), [chosenProductID],function(error, results, fields) {
+    case chosenProductID:
+      connection.query(sqlSel, function(error, results, fields) {
+        if(error) throw error;
+        let productCost = parseFloat(chosenQty * results[0].price);
         console.log(results);
         if(results[0].stock_quantity < chosenQty) {
           console.log('We are sorry - there are only ' + results[0].stock_quantity + ' of your chosen item available');
         } else {
-          console.log('you purchased ' + chosenQty + ' ' + chosenProductID + 's');
-        }break;
-      }
-  }
-})
-}
-
-intializeStore();
-  
-  function intializeStore2() {
-  productList();
-  inquirer.prompt([
-    {
-      name: 'buySelection',
-      type: 'input',
-      message: 'Using the product sheet, choose an item to purchase by selecting its unique ID number'
-    },
-    {
-      name: 'quantitySelection',
-      type: 'input',
-      message: 'Great choice! How many of them would you like?'
-    }
-  ]).then(function(answer) {
-    let chosenProduct = answer.buySelection;
-    let chosenQuantity = answer.quantitySelection;
-    if(chosenProduct === 1) {
-      connection.query('SELECT * FROM products WHERE item_id =' + chosenProduct), function (error, results, fields) {
-        console.log(results);
-        console.log(fields);
-      }
+          console.log('You purchased ' + chosenQty + ' ' + results[0].product_name + 's');
+          console.log('The total cost of your purchase is $' + productCost + ' USD!');
+          let newQty = results[0].stock_quantity - chosenQty;
+          let sqlUpd = 'UPDATE products SET stock_quantity = ' + newQty + ' WHERE item_id = ' + connection.escape(chosenProductID);
+          connection.query(sqlUpd, function(error, results, fields) {
+            if(error) throw error;
+            console.log('There are now ' + newQty + ' ' + results[0].product_name + 's left in stock!');
+          })
+          inquirer.prompt([
+            {
+              name: 'postPurchase',
+              type: 'list',
+              message: 'Would you like to make another purchase?',
+              choices: ['yes', 'no']
+            }
+          ]).then(function(answer) {
+            if(answer.postPurchase === 'yes') {
+              initializeStore();
+            } else {
+              console.log('Thank you for shopping at Bamazon!');
+              connection.end();
+            }
+          })
+        }
+      })
     }
   })
-  }
-  
-  // intializeStore2();
+}
+
+productList();
+
